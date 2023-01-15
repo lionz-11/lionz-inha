8de48736680f68e959991728b9a2c3a422971ebc
pakxe <pigkill40@naver.com>
2023-01-15 22:19:26 +0900

    feat(GlobalStyle): 글로벌로 폰트 적용

--- src/GlobalStyle.jsx
+++ src/GlobalStyle.jsx
@@ -1,11 +1,32 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'Pretendard';
+    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1Pretendard-Bold.woff') format('woff');
+    font-weight: 700;
+  }
+
+  @font-face {
+    font-family: 'Pretendard';
+    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-SemiBold.woff') format('woff');
+    font-weight: 600;
+  }
+
+  @font-face {
+    font-family: 'Pretendard';
+    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
+    font-weight: 400;
+  }
+  
+
+  
   * {
+    font-family: 'Pretendard';
     box-sizing: border-box;
     outline: none;
     margin: 0;
-    padding: 0;
+    padding: 40px;
   }
 `;
 
