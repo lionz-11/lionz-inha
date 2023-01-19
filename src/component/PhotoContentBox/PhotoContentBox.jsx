616ba45eeb77cde4bac2a278373270f58d120f07
pakxe <pigkill40@naver.com>
2023-01-20 02:48:56 +0900

    chore(PhotoContentBox): PhotoContentBox 틀 제작

---
+++ src/component/PhotoContentBox/PhotoContentBox.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Container = styled.article`
+  height: 418px;
+  width: 416px;
+  border: 1px solid black;
+`;
+
+const PhotoContentBox = () => <Container>hi</Container>;
+
+export default PhotoContentBox;
