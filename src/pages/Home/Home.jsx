056e717387f0819e2890446f93ba26ebc8c6f6e7
pakxe <pigkill40@naver.com>
2023-01-19 21:41:13 +0900

    chore(Home): PageTitle 컴포넌트 사용 예시 작성

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,27 +1,11 @@
-import Button from '../../component/Button/Button';
-import Flex from '../../component/Flex/Flex';
-import Typography from '../../component/Typography/Typography';
+import PageTitle from '../../component/PageTitle/PageTitle';
 
 const Home = () => (
-  <>
-    <Typography header>hi</Typography>
-    <Typography semiHeader>hi</Typography>
-    <Typography contentTitle>hi</Typography>
-    <Typography small>hi</Typography>
-    <Flex justify='space-between'>
-      <h1>h1</h1>
-      <h1>h1</h1>
-    </Flex>
-    <Button>🙏 새 글 작성</Button>
-    <Button color='darkGray'>🙏 새 글 작성</Button>
-    <Button small>✔️ Today</Button>
-    <Button textButton color='red'>
-      작성
-    </Button>
-    <Button textButtonSmall color='blue'>
-      추가
-    </Button>
-  </>
+  <PageTitle>
+    페이지
+    <span> 타이틀 </span>
+    입니다.
+  </PageTitle>
 );
 
 export default Home;
