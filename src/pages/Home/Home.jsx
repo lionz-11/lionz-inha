7e868e513e574311fc8aad6f4a415b3922836bb3
pakxe <pigkill40@naver.com>
2023-01-23 04:08:29 +0900

    chore(Home): BarContentBox 컴포넌트 사용 예시 작성

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,33 +1,15 @@
 import Layout from '../../component/Layout/Layout';
 import Header from '../../component/Header/Header';
 import Footer from '../../component/Footer/Footer';
-import PhotoContentContainer from '../../component/PhotoContentBox/PhotoContentContainer';
-
-const dummy = {
-  title: '이건 과제 제목입니다',
-  contents:
-    '이건 과제 소개 입니다.이건 과제 소개 입니다.이건 과제 니다.이건 과제 니다.이건 과제 니다.이건 과제 니다.이건과제 니다.이건 과제 니다.이건 과제 니다.이건 과제 니다.이건 과제 소개 입니다.이건 과제 소개 입니다.이건',
-  writer: 'FE',
-  date: '마감일: 2001년 03월 24일',
-};
-
-const a = new Array(2).fill(0).map((_, i) => ({ ...dummy, id: i }));
+import BarContentBox from '../../component/BarContentBox/BarContentBox';
 
 const Home = () => (
   <Layout>
     <Header />
     <Footer />
-    <PhotoContentContainer haveProfile data={a} />
-    <PhotoContentContainer haveProfile data={a} />
-    <PhotoContentContainer haveProfile data={a} />
-    <PhotoContentContainer haveProfile data={a} />
-    <PhotoContentContainer haveProfile data={a} />
-    <PhotoContentContainer haveProfile data={a} />
-    <PhotoContentContainer haveProfile data={a} />
-    <PhotoContentContainer haveProfile data={a} />
-    <PhotoContentContainer haveProfile data={a} />
-    <PhotoContentContainer haveProfile data={a} />
-    <PhotoContentContainer haveProfile data={a} />
+    <BarContentBox tag='FE' title='이건 과제임' assignment date='03월 24일 18시' submissionStatus='제출 완료' />
+    <BarContentBox tag='FE' title='이건 과제임' assignment date='03월 24일 18시' submissionStatus='대상 아님' />
+    <BarContentBox tag='FE' title='이건 과제임' assignment date='03월 24일 18시' submissionStatus='미제출' />
   </Layout>
 );
 
