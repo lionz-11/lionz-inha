ed7ebf6b2acef059ced47d3fd8f4a146075d1238
pakxe <pigkill40@naver.com>
2023-01-23 16:25:16 +0900

    chore(Home): BarContentBox 컴포넌트의 프로필 이미지를 적용하는 예제 작성

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,14 +2,22 @@ import Layout from '../../component/Layout/Layout';
 import Header from '../../component/Header/Header';
 import Footer from '../../component/Footer/Footer';
 import BarContentBox from '../../component/BarContentBox/BarContentBox';
+// https://blog.kakaocdn.net/dn/dpxiAT/btqUBv6Fvpn/E8xUMncq7AVuDeOim0LrMk/img.jpg
 
 const Home = () => (
   <Layout>
     <Header />
     <Footer />
-    <BarContentBox tag='FE' title='이건 과제임' assignment date='03월 24일 18시' submissionStatus='제출 완료' />
-    <BarContentBox tag='FE' title='이건 과제임' assignment date='03월 24일 18시' submissionStatus='대상 아님' />
-    <BarContentBox tag='FE' title='이건 과제임' assignment date='03월 24일 18시' submissionStatus='미제출' />
+    <BarContentBox
+      tag='FE'
+      title='이건 과제임'
+      date='03월 24일 18시'
+      submissionStatus='제출 완료'
+      writer='박사자'
+      maximum='6'
+      current='3'
+      img='https://blog.kakaocdn.net/dn/dpxiAT/btqUBv6Fvpn/E8xUMncq7AVuDeOim0LrMk/img.jpg'
+    />
   </Layout>
 );
 
