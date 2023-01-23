37b5b1ac2f9ba355628951685d339a92f29d72e5
pakxe <pigkill40@naver.com>
2023-01-23 17:52:59 +0900

    chore(Home): 사진 적용한 PhotoContentBox 적용 예시 작성

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,24 @@
 import Layout from '../../component/Layout/Layout';
-import Header from '../../component/Header/Header';
-import Footer from '../../component/Footer/Footer';
 import BarContentBox from '../../component/BarContentBox/BarContentBox';
+import PhotoContentContainer from '../../component/PhotoContentBox/PhotoContentContainer';
 // https://blog.kakaocdn.net/dn/dpxiAT/btqUBv6Fvpn/E8xUMncq7AVuDeOim0LrMk/img.jpg
+
+const dummy = {
+  title: '이건 과제 제목입니다',
+  contents:
+    '이건 과제 소개 입니다.이건 과제 소개 입니다.이건 과제 니다.이건 과제 니다.이건 과제 니다.이건 과제 니다.이건과제 니다.이건 과제 니다.이건 과제 니다.이건 과제 니다.이건 과제 소개 입니다.이건 과제 소개 입니다.이건',
+  writer: 'FE',
+  date: '마감일: 2001년 03월 24일',
+  profileImg: 'https://blog.kakaocdn.net/dn/dpxiAT/btqUBv6Fvpn/E8xUMncq7AVuDeOim0LrMk/img.jpg',
+  photo: 'https://blog.kakaocdn.net/dn/dpxiAT/btqUBv6Fvpn/E8xUMncq7AVuDeOim0LrMk/img.jpg',
+};
+
+const a = new Array(2).fill(0).map((_, i) => ({ ...dummy, id: i }));
 
 const Home = () => (
   <Layout>
-    <Header />
-    <Footer />
+    <PhotoContentContainer haveProfile data={a} />
+    <PhotoContentContainer data={a} />
     <BarContentBox
       tag='FE'
       title='이건 과제임'
