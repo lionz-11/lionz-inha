ff6e16fe720d20bd9839800b947b49bd5a01e18c
pakxe <pigkill40@naver.com>
2023-01-23 19:14:16 +0900

    chore(Home): SemiTitle 사용 예시 작성

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,34 +1,9 @@
 import Layout from '../../component/Layout/Layout';
-import BarContentBox from '../../component/BarContentBox/BarContentBox';
-import PhotoContentContainer from '../../component/PhotoContentBox/PhotoContentContainer';
-// https://blog.kakaocdn.net/dn/dpxiAT/btqUBv6Fvpn/E8xUMncq7AVuDeOim0LrMk/img.jpg
-
-const dummy = {
-  title: '이건 과제 제목입니다',
-  contents:
-    '이건 과제 소개 입니다.이건 과제 소개 입니다.이건 과제 니다.이건 과제 니다.이건 과제 니다.이건 과제 니다.이건과제 니다.이건 과제 니다.이건 과제 니다.이건 과제 니다.이건 과제 소개 입니다.이건 과제 소개 입니다.이건',
-  writer: 'FE',
-  date: '마감일: 2001년 03월 24일',
-  profileImg: 'https://blog.kakaocdn.net/dn/dpxiAT/btqUBv6Fvpn/E8xUMncq7AVuDeOim0LrMk/img.jpg',
-  photo: 'https://blog.kakaocdn.net/dn/dpxiAT/btqUBv6Fvpn/E8xUMncq7AVuDeOim0LrMk/img.jpg',
-};
-
-const a = new Array(2).fill(0).map((_, i) => ({ ...dummy, id: i }));
+import SemiTitle from '../../component/SemiTitle/SemiTitle';
 
 const Home = () => (
   <Layout>
-    <PhotoContentContainer haveProfile data={a} />
-    <PhotoContentContainer data={a} />
-    <BarContentBox
-      tag='FE'
-      title='이건 과제임'
-      date='03월 24일 18시'
-      submissionStatus='제출 완료'
-      writer='박사자'
-      maximum='6'
-      current='3'
-      img='https://blog.kakaocdn.net/dn/dpxiAT/btqUBv6Fvpn/E8xUMncq7AVuDeOim0LrMk/img.jpg'
-    />
+    <SemiTitle>나의 과제</SemiTitle>
   </Layout>
 );
 
