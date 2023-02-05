a0df5658722a663ce8a5158e110e6aaca0910806
pakxe <pigkill40@naver.com>
2023-02-06 01:17:09 +0900

    chore(Home): LikeAndShare 사용 예시 작성

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,35 +1,22 @@
 import { useState } from 'react';
 import Layout from '../../component/Layout/Layout';
-import AssignmentBox from '../../component/AssignmentBox/AssignmentBox';
-import PhotoContentContainer from '../../component/PhotoContentBox/PhotoContentContainer';
-import TextButton from '../../component/TextButton/TextButton';
+import LikeAndShare from '../../component/LikeAndShare/LikeAndShare';
 
-const dummy = {
-  title: '이건 과제 제목입니다',
-  contents:
-    '이건 과제 소개 입니다.이건 과제 소개 입니다.이건 과제 니다.이건 과제 니다.이건 과제 니다.이건 과제 니다.이건과제 니다.이건 과제 니다.이건 과제 니다.이건 과제 니다.이건 과제 소개 입니다.이건 과제 소개 입니다.이건',
-  writer: 'FE',
-  date: '마감일: 2001년 03월 24일',
-};
+// const dummy = {
+//   title: '이건 과제 제목입니다',
+//   contents:
+//     '이건 과제 소개 입니다.이건 과제 소개 입니다.이건 과제 니다.이건 과제 니다.이건 과제 니다.이건 과제 니다.이건과제 니다.이건 과제 니다.이건 과제 니다.이건 과제 니다.이건 과제 소개 입니다.이건 과제 소개 입니다.이건',
+//   writer: 'FE',
+//   date: '마감일: 2001년 03월 24일',
+// };
 
-const a = new Array(2).fill(0).map((_, i) => ({ ...dummy, id: i }));
+// const a = new Array(2).fill(0).map((_, i) => ({ ...dummy, id: i }));
 const Home = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [like, setLike] = useState(false);
 
   return (
     <Layout>
-      <PhotoContentContainer data={a} />
-      <AssignmentBox
-        date='3월 24일 13시 59분'
-        src='https://blog.kakaocdn.net/dn/dpxiAT/btqUBv6Fvpn/E8xUMncq7AVuDeOim0LrMk/img.jpg'
-        name='박세현'
-        detail='sss'
-        link='https://www.naver.com'
-        isOpen={isOpen}
-        setIsOpen={setIsOpen}
-      />
-      <TextButton haveDelete />
-      <TextButton />
+      <LikeAndShare like={like} setLike={() => setLike(!like)} />
     </Layout>
   );
 };
