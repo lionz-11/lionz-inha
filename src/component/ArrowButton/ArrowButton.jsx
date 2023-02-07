5e64facce78ac38e8365c30d5e1e75c2a9e888ba
DongRyeong Lee <dr11291129@gmail.com>
2023-02-07 19:24:05 +0900

    comment(ArrowButton): 주석 작성

--- src/component/ArrowButton/ArrowButton.jsx
+++ src/component/ArrowButton/ArrowButton.jsx
@@ -3,6 +3,7 @@ import { BsChevronRight } from 'react-icons/bs';
 import Flex from '../Flex/Flex';
 import Typography from '../Typography/Typography';
 
+// 사이즈 조절을 위해 props로 전달받아 내부적으로 값 지정
 const StyledTypo = styled(Typography)`
   font-size: ${(props) => (props.size === 'small' ? 14 : 16)}px;
 `;
