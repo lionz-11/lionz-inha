003be5a9f7ecba4a71469452172943b5be79e9de
pakxe <pigkill40@naver.com>
2023-02-09 03:04:14 +0900

    feat(Routes): ProfileEdit 페이지 라우터 연결

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home/Home';
+import ProfileEdit from '../pages/ProfileEdit/ProfileEdit';
 
 const Router = () => (
   <BrowserRouter>
     <Routes>
-      <Route exact path='/' element={<Home />} />
+      <Route path='/' element={<Home />} />
+      <Route path='/profile-edit' element={<ProfileEdit />} />
     </Routes>
   </BrowserRouter>
 );
