167c5a1f3d73050c53fb9d831ba9d12f2c6bb5cd
pakxe <pigkill40@naver.com>
2023-03-04 04:23:17 +0900

    feat(TitleContainer): 반복되는 형태로 사용되던 제목+컴포넌트를 TitleContainer 컴포넌트를 제작해 묶음

---
+++ src/component/TitleContainer/TitleContainer.jsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+import TitleSet from '../TitleSet/TitleSet';
+
+const Container = styled.div`
+  width: ${({ width }) => (width ? `${width}px` : '100%')};
+  display: flex;
+  flex-direction: column;
+  gap: 18px;
+`;
+
+const TitleContainer = ({ width, mainTitle, subTitle, component, small }) => (
+  <Container width={width}>
+    <TitleSet small={small} mainTitle={mainTitle} subTitle={subTitle} />
+    {component}
+  </Container>
+);
+
+export default TitleContainer;
