63ea761e908e8ac59a9dae077670a98085ba7c6b
pakxe <pigkill40@naver.com>
2023-03-11 15:29:06 +0900

    feat(Layout): Layout 하단 여백 추가

--- src/component/Layout/Layout.jsx
+++ src/component/Layout/Layout.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import Margin from '../Margin/Margin';
 
 const Width = styled.div`
   margin: 0 auto;
@@ -20,7 +21,10 @@ const Background = styled.div`
 
 const Layout = ({ children, size }) => (
   <Background>
-    <Width size={size}>{children}</Width>
+    <Width size={size}>
+      {children}
+      <Margin height='304' />
+    </Width>
   </Background>
 );
 
