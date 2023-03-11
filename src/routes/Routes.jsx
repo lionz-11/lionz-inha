064942d46a6850051d09952ad10598b08a443c8b
pakxe <pigkill40@naver.com>
2023-03-11 15:32:33 +0900

    feat(Routes): HomeWorkList 페이지 연결

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home/Home';
 import ProfileEdit from '../pages/ProfileEdit/ProfileEdit';
 import Landing from '../pages/Landing/Landing';
+import HomeworkList from '../pages/HomeworkList/HomeworkList';
 
 const Router = () => (
   <BrowserRouter>
@@ -9,6 +10,7 @@ const Router = () => (
       <Route path='/' element={<Home />} />
       <Route path='/profile-edit' element={<ProfileEdit />} />
       <Route path='/landing' element={<Landing />} />
+      <Route path='/homework-list' element={<HomeworkList />} />
     </Routes>
   </BrowserRouter>
 );
